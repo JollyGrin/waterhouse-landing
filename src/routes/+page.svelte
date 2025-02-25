9ac8c2d753df6d07ed280955152e6e9141e08c6f
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-25 23:28:56 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,23 @@
 <div class="grid min-h-screen place-items-center bg-teal-100 px-6">
 	<div class="w-full rounded-lg border-3 border-black bg-white p-4">
 		<div class="grid grid-cols-[1fr_1fr_4fr_1fr_1fr] gap-2">
-			<div class="">
-				<div
-					class="shadow-flat hover:shadow-hover hover:translate-reset translate-adjust rounded border-2 border-black transition-all"
-				>
-					hi
-				</div>
+			<div class=" grid grid-rows-[2rem_10rem] gap-2">
+				<div class="midi">hi</div>
+				<div class="bg-black">hi</div>
 			</div>
-			<div>
-				<div class="midi">hdjksa</div>
+			<div class="grid grid-rows-[2rem_10rem] gap-2">
+				<div class="midi">hi</div>
+				<div class="bg-black">hi</div>
 			</div>
 			<div class="bg-black">x</div>
-			<div class="bg-black">x</div>
-			<div class="bg-black">x</div>
+			<div class=" grid grid-rows-[2rem_10rem] gap-2">
+				<div class="midi">hi</div>
+				<div class="bg-black">hi</div>
+			</div>
+			<div class=" grid grid-rows-[2rem_10rem] gap-2">
+				<div class="midi">hi</div>
+				<div class="bg-black">hi</div>
+			</div>
 		</div>
 	</div>
 </div>
-
-<style>
-</style>
