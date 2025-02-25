0b8a816ccb4c607819d5f436b4d38c32d603d49d
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-25 23:55:09 +0100

    add turntable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,14 @@
 				<div class="midi h-full"></div>
 				<div class="midi h-full"></div>
 			</div>
-			<div class="bg-black">TODO: put turntable here</div>
+			<div class="turntable-container bg-black flex items-center justify-center p-4">
+				<div class="turntable">
+					<div class="record">
+						<div class="label"></div>
+					</div>
+					<div class="tonearm"></div>
+				</div>
+			</div>
 			<div class="flex flex-col gap-2">
 				<div class="midi h-full"></div>
 				<div class="midi h-full"></div>
@@ -26,3 +33,70 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	.turntable-container {
+		aspect-ratio: 1;
+	}
+
+	.turntable {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		background: #333;
+		border-radius: 10px;
+		padding: 2rem;
+	}
+
+	.record {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		background: #111;
+		border-radius: 50%;
+		animation: spin 4s linear infinite;
+	}
+
+	.label {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 30%;
+		height: 30%;
+		background: #666;
+		border-radius: 50%;
+	}
+
+	.tonearm {
+		position: absolute;
+		top: 10%;
+		right: 10%;
+		width: 40%;
+		height: 4px;
+		background: #666;
+		transform-origin: right center;
+		transform: rotate(-20deg);
+	}
+
+	.tonearm::after {
+		content: '';
+		position: absolute;
+		left: 0;
+		top: 50%;
+		transform: translateY(-50%);
+		width: 8px;
+		height: 8px;
+		background: #666;
+		border-radius: 50%;
+	}
+
+	@keyframes spin {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+</style>
