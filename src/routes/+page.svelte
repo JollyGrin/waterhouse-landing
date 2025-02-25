27f62ca5c8bf5187c62394a8d7e059a6257670df
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-25 23:53:41 +0100

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,21 +5,23 @@
 	<div class="w-full rounded-lg border-3 border-black bg-white p-4">
 		<div class="grid grid-cols-[1fr_1fr_4fr_1fr_1fr] gap-2">
 			<div class=" grid grid-rows-[2rem_10rem] gap-2">
-				<div class="midi">hi</div>
-				<div class="bg-black">hi</div>
+				<div class="midi"></div>
+				<div class="rounded-lg border-2 border-black"></div>
 			</div>
-			<div class="grid grid-rows-[2rem_10rem] gap-2">
-				<div class="midi">hi</div>
-				<div class="bg-black">hi</div>
+			<div class="flex flex-col gap-2">
+				<div class="midi h-full"></div>
+				<div class="midi h-full"></div>
+				<div class="midi h-full"></div>
 			</div>
-			<div class="bg-black">x</div>
+			<div class="bg-black">TODO: put turntable here</div>
+			<div class="flex flex-col gap-2">
+				<div class="midi h-full"></div>
+				<div class="midi h-full"></div>
+				<div class="midi h-full"></div>
+			</div>
 			<div class=" grid grid-rows-[2rem_10rem] gap-2">
-				<div class="midi">hi</div>
-				<div class="bg-black">hi</div>
-			</div>
-			<div class=" grid grid-rows-[2rem_10rem] gap-2">
-				<div class="midi">hi</div>
-				<div class="bg-black">hi</div>
+				<div class="midi"></div>
+				<div class="rounded-lg border-2 border-black"></div>
 			</div>
 		</div>
 	</div>
