b5ff77999bc101683d532c3796675a742babfb1a
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-26 04:18:43 +0100

    fix overflow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 <ModalServices isOpen={isOpen === 'services'} {onClose} />
 
 <div
-	class="font-jersey grid gap-4 bg-teal-950 px-4 pt-4 md:min-h-screen md:place-items-center md:gap-0 md:px-6 md:pt-0"
+	class="font-jersey grid w-screen gap-4 overflow-hidden bg-teal-950 px-4 pt-4 md:min-h-screen md:place-items-center md:gap-0 md:px-6 md:pt-0"
 >
 	<div class="grid h-full min-h-30 place-items-center md:hidden">
 		<div class="w-20 text-teal-600">
