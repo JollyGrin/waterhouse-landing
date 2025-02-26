d2deacd01db4c4755913e9fff209e60f5765c2b6
Dean Masley <grins@Deans-MacBook-Pro.local>
2025-02-26 01:30:05 +0100

    clean up

---
+++ src/lib/MidiSlider.svelte
@@ -0,0 +1,64 @@
+<input type="range" min="0" max="100" value="0" class="range-slider" id="color-slider" />
+
+<style>
+	input[type='range'] {
+		height: 100%;
+		writing-mode: vertical-lr;
+		direction: rtl;
+		appearance: slider-vertical;
+		width: 16px;
+		vertical-align: bottom;
+		background: transparent;
+	}
+
+	/* Track styles */
+	input[type='range']::-webkit-slider-runnable-track {
+		width: 100%;
+		background: #134e4a;
+		border: 1px solid #0f766e;
+		border-radius: 4px;
+	}
+
+	input[type='range']::-moz-range-track {
+		width: 100%;
+		background: #134e4a;
+		border: 1px solid #0f766e;
+		border-radius: 4px;
+	}
+
+	/* Thumb (dot) styles */
+	input[type='range']::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		height: 16px;
+		width: 16px;
+		border-radius: 50%;
+		background: #14b8a6;
+		border: 2px solid #0d9488;
+		margin-top: -4px;
+		cursor: pointer;
+	}
+
+	input[type='range']::-moz-range-thumb {
+		height: 16px;
+		width: 16px;
+		border-radius: 50%;
+		background: #14b8a6;
+		border: 2px solid #0d9488;
+		cursor: pointer;
+	}
+
+	/* Focus styles */
+	input[type='range']:focus {
+		outline: none;
+	}
+
+	input[type='range']:focus::-webkit-slider-thumb {
+		background: #2dd4bf;
+		border-color: #14b8a6;
+	}
+
+	input[type='range']:focus::-moz-range-thumb {
+		background: #2dd4bf;
+		border-color: #14b8a6;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import MidiScreen from '$lib/MidiScreen.svelte';
+	import MidiSlider from '$lib/MidiSlider.svelte';
 	import SpeakerGrate from '$lib/SpeakerGrate.svelte';
 	import Turntable from '$lib/Turntable.svelte';
 </script>
@@ -12,7 +13,7 @@
 				<div
 					class="hidden max-h-[300px] items-center justify-center rounded-lg border-2 border-black p-2 md:flex"
 				>
-					<input type="range" min="0" max="100" value="0" class="range-slider" id="color-slider" />
+					<MidiSlider />
 				</div>
 
 				<SpeakerGrate />
@@ -35,66 +36,3 @@
 		</div>
 	</div>
 </div>
-
-<style>
-	input[type='range'] {
-		height: 100%;
-		writing-mode: vertical-lr;
-		direction: rtl;
-		appearance: slider-vertical;
-		width: 16px;
-		vertical-align: bottom;
-		background: transparent;
-	}
-
-	/* Track styles */
-	input[type='range']::-webkit-slider-runnable-track {
-		width: 100%;
-		background: #134e4a;
-		border: 1px solid #0f766e;
-		border-radius: 4px;
-	}
-
-	input[type='range']::-moz-range-track {
-		width: 100%;
-		background: #134e4a;
-		border: 1px solid #0f766e;
-		border-radius: 4px;
-	}
-
-	/* Thumb (dot) styles */
-	input[type='range']::-webkit-slider-thumb {
-		-webkit-appearance: none;
-		height: 16px;
-		width: 16px;
-		border-radius: 50%;
-		background: #14b8a6;
-		border: 2px solid #0d9488;
-		margin-top: -4px;
-		cursor: pointer;
-	}
-
-	input[type='range']::-moz-range-thumb {
-		height: 16px;
-		width: 16px;
-		border-radius: 50%;
-		background: #14b8a6;
-		border: 2px solid #0d9488;
-		cursor: pointer;
-	}
-
-	/* Focus styles */
-	input[type='range']:focus {
-		outline: none;
-	}
-
-	input[type='range']:focus::-webkit-slider-thumb {
-		background: #2dd4bf;
-		border-color: #14b8a6;
-	}
-
-	input[type='range']:focus::-moz-range-thumb {
-		background: #2dd4bf;
-		border-color: #14b8a6;
-	}
-</style>
