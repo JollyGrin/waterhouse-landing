60b544e6b3459ac393ced6ba4ff27d994b5b15e2
JollyGrin <lol@dean.lol>
2025-05-12 02:44:23 +0200

    progress

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,31 +1,78 @@
 <script lang="ts">
 </script>
 
-<div class="container mx-auto mt-10 min-h-[300px] w-full border-2">
+<div
+	class="container mx-auto mt-10 min-h-[300px] w-full overflow-clip rounded-xl border-2 bg-slate-50 p-2"
+>
 	<div class="screen bg-gray-200"></div>
 	<div class="news"></div>
-	<div class="slider"></div>
-	<div class="location"></div>
-	<div class="contact"></div>
-	<div class="rec"></div>
-	<div class="play"></div>
-	<div class="volume"></div>
-	<div class="social-1"></div>
-	<div class="social-2"></div>
-	<div class="social-3"></div>
-	<div class="social-4"></div>
-	<div class="special"></div>
-	<div class="sample"></div>
-	<div class="drum"></div>
+	<div class="slider">
+		<div class="h-full w-full rounded-lg border-2"></div>
+	</div>
+	<div class="location">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="contact">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="rec grid place-items-center">
+		<button class="rounded-full"> x </button>
+	</div>
+	<div class="play">
+		<button class="rounded-full"> x </button>
+	</div>
+	<div class="volume grid place-items-center">
+		<div class="relative h-20 w-20 rounded-full border-2 bg-white shadow-[2px_2px_0_black]">
+			<div class="absolute left-[49%] h-10 w-1 rounded-b-full border-l-4"></div>
+		</div>
+	</div>
+	<div class="social-1">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="social-2">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="social-3">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="social-4">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="special">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="sample">
+		<button class="rounded-lg"> x </button>
+	</div>
+	<div class="drum">
+		<button class="rounded-lg"> x </button>
+	</div>
 	<div class="speaker"></div>
 </div>
 
 <style>
+	:root {
+		--ctrl-unit: calc(calc(100vw - calc(60 * 2px)) / 89.66);
+		--ctrl-grid-height: calc(var(--ctrl-unit) * 10);
+	}
+	button {
+		width: 100%;
+		height: 100%;
+		border: solid 2px black;
+		box-shadow: 3px 3px 0 black;
+		transition: all 0.25s ease;
+		background-color: var(--color-white);
+	}
+	button:hover {
+		transform: translate(2px, 2px);
+		box-shadow: 1px 1px 0px black;
+	}
+
 	.container {
 		display: grid;
 		grid-template-columns: repeat(8, 1fr);
-		grid-template-rows: repeat(4, 176px);
-		gap: 0px 0px;
+		grid-template-rows: repeat(4, var(--ctrl-grid-height));
+		gap: 8px;
 		grid-auto-flow: row;
 		grid-template-areas:
 			'location contact screen screen screen screen special speaker'
